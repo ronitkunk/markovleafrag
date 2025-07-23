5564a797ae1269802754975879071a106b685f3a
Ronit Kunkolienker <ronitkunk@gmail.com>
2025-07-23 16:14:24 +0530

    softmax

--- markov_composer.py
+++ markov_composer.py
@@ -26,21 +26,22 @@ Visualises a Markov chain.
 Plots the Markov chain states as a directed graph, with edge weights (for first order chains only) representing conditional probabilities of the transition from source chord to sink chord.
 The vertex for `highlighted_chord` is shown in a different colour.
 """
-def plot_graph_from_adj(adj: dict, previous_chords: list[str] = [], current_chord: str = None, mode: str = None):
+def plot_graph_from_adj(adj: dict, previous_chords: list[str] = [], current_chord: str = None, mode: str = None, temperature: float = 1.0):
     G = nx.DiGraph()
     for curr, targets in adj.items():
         for next, freq in targets.items():
-            G.add_edge(curr[-1], next, weight=freq / sum(targets.values()))
+            G.add_edge(curr[-1], next, weight=(np.exp(np.array(freq)/temperature) / np.sum(np.exp(np.array(list(targets.values()))/temperature))) if mode=="simulate" else freq)
 
     pos = nx.spiral_layout(G)
     edge_labels = nx.get_edge_attributes(G, 'weight')
     node_colors = [("black" if node==current_chord else ("teal" if node in previous_chords else "aqua")) for node in G.nodes()] if mode=="fit" else [("black" if node==current_chord else ("firebrick" if node in previous_chords else "lightcoral")) for node in G.nodes()]
 
     plt.clf()
-    nx.draw(G, pos, node_color=node_colors, arrows=True)
+    nx.draw(G, pos, node_color=node_colors, arrows=True, with_labels=False)
     if len(list(adj.keys())[0])==1:
-        nx.draw_networkx_edge_labels(G, pos, edge_labels={k: f"{v:.2f}" for k, v in edge_labels.items()})
+        nx.draw_networkx_edge_labels(G, pos, edge_labels={k: (f"{v:.2f}" if mode=="simulate" else f"{int(v)}") for k, v in edge_labels.items()})
     plt.pause(0.3)
+    # plt.show()
 
 """
 FOR look_back=1:
@@ -50,12 +51,12 @@ The Markov chain is stored as a dict whose:
 - key is a 1-tuple containing the previous chord as a string
 - value is a dict of (next chord : conditional frequency) pairs
 
-It learns the probability distribution of the next chord conditional on the previous chord.
+It learns the frequency distribution of the next chord conditional on the previous chord.
 
 FOR look_back>1:
 Returns an n-gram model (n=look_back), which does not have the Markov property. No visualisation is performed.
 """
-def fit_markov_chain(filename: str, look_back: int = 1, play: bool=False, plot: bool=False) -> dict:
+def fit_markov_chain(filename: str, look_back: int = 1, play: bool=False, plot: bool=False, temperature: float=1.0) -> dict:
     infile = open(filename, "r")
     text = infile.read()
     infile.close()
@@ -63,12 +64,13 @@ def fit_markov_chain(filename: str, look_back: int = 1, play: bool=False, plot: 
     chord_sequence = text.split()
     adj = {}
     iters = len(chord_sequence)-look_back
+
     for i in range(iters):
         if tuple(chord_sequence[i:i+look_back]) not in adj.keys():
             adj[tuple(chord_sequence[i:i+look_back])] = {}
 
         # update conditional frequency
-        adj[tuple(chord_sequence[i:i+look_back])][chord_sequence[i+look_back]] = adj[tuple(chord_sequence[i:i+look_back])].get(chord_sequence[i+look_back], 0) + 1
+        adj[tuple(chord_sequence[i:i+look_back])][chord_sequence[i+look_back]] = adj[tuple(chord_sequence[i:i+look_back])].get(chord_sequence[i+look_back], 0)+ 1
 
         if play:
             sd.play(make_chord([int(x) for x in chord_sequence[i+look_back].split(',')], samplerate=44100, duration=0.2), samplerate=44100)
@@ -76,32 +78,52 @@ def fit_markov_chain(filename: str, look_back: int = 1, play: bool=False, plot: 
                 sd.wait()
 
         if plot:
-            plot_graph_from_adj(adj, previous_chords=chord_sequence[i:i+look_back], current_chord=chord_sequence[i+look_back], mode="fit")
+            plot_graph_from_adj(adj, previous_chords=chord_sequence[i:i+look_back], current_chord=chord_sequence[i+look_back], mode="fit", temperature=temperature)
 
-        print(f"[fitting {((i+1)*100)//iters}%] For chord(s) {tuple(chord_sequence[i:i+look_back])}: updated probability of transition to {chord_sequence[i+look_back]} to {(adj[tuple(chord_sequence[i:i+look_back])][chord_sequence[i+look_back]])/sum(adj[tuple(chord_sequence[i:i+look_back])].values()):.4f}")
-        
+        print(f"[fitting {((i+1)*100)//iters}%] For chord(s) {tuple(chord_sequence[i:i+look_back])}: updated frequency of transition to {chord_sequence[i+look_back]} to {(adj[tuple(chord_sequence[i:i+look_back])][chord_sequence[i+look_back]])}")
+    
+    # Save an empty distribution for last key if the key did not appear before
+    if tuple(chord_sequence[len(chord_sequence)-look_back:len(chord_sequence)]) not in adj.keys():
+        adj[tuple(chord_sequence[len(chord_sequence)-look_back:len(chord_sequence)])] = {}
+    # Build all the remaining edges with weight 0 to create a fully connected adjacency graph; even the "0" frequency transitions have nonzero probability softmaxing the conditional frequency distribution
+    for i in range(iters+1):
+        for chord in chord_sequence:
+            if chord not in adj[tuple(chord_sequence[i:i+look_back])].keys():
+                adj[tuple(chord_sequence[i:i+look_back])][chord] = 0
+
+    # # see transition matrix
+    # for key in adj.keys():
+    #     print(f"{key} --- {list(adj[key].values())}")
     return adj
 
 """
 [NOTE: The following description is for a Markov chain, but the function will also work for an n-gram model]
 Given a fitted Markov chain as described above, composes and plays a new piece starting from a random chord.
 
-For each 'current chord', it will sample a new chord from the conditional distribution of the next chord given the current chord, which is assumed to have already been learned.
+For each 'current chord', it will sample a new chord from the conditional distribution of the next chord given the current chord, which is generated by applying a softmax over the frequencies, instead of simple normalisation.
 
 Crashes if it encounters a dead-end, i.e., there is no possible new chord.
 """
-def compose_piece(markov_chain: dict, piece_length: int = 1000, save_path: str = "output.wav", play: bool=True, plot: bool=True):
+def compose_piece(markov_chain: dict, piece_length: int = 1000, save_path: str = "output.wav", play: bool = True, plot: bool = True, save: bool = True, temperature: float = 1.0):
     audio_sequence = []
     prev_state = list(markov_chain.keys())[0]
 
     for i in range(piece_length):
-        # simulate the conditional distribution
-        current_chord = random.choices(list(markov_chain[prev_state].keys()), weights=list(markov_chain[prev_state].values()))[0]
+        # use conditional frequencies as logits instead of un-normalised probabilities, this provides smoothing while preserving the true distribution better than +1 smoothing
+        freqs = list(markov_chain[prev_state].values())
+        logits = np.array(freqs) / temperature
+        pmf = np.exp(logits) / np.sum(np.exp(logits))
 
-        print(f"[generating {((i+1)*100)//piece_length}%] Transition from", prev_state, "to", current_chord)
+        # simulate the "approximate" (softmaxed) conditional distribution
+        current_chord = random.choices(list(markov_chain[prev_state].keys()), weights=pmf)[0]
 
-        chord = make_chord([int(x) for x in current_chord.split(',')], samplerate=44100, duration=0.2)
-        audio_sequence.append(chord)
+        # print(f"[generating {((i+1)*100)//piece_length}%] Transition from", prev_state, "to", current_chord)
+        print(current_chord, end=" ")
+
+        chord = None
+        if save or play:
+            chord = make_chord([int(x) for x in current_chord.split(',')], samplerate=44100, duration=0.2)
+            audio_sequence.append(chord)
 
         if play:
             sd.play(chord, samplerate=44100)
@@ -109,11 +131,12 @@ def compose_piece(markov_chain: dict, piece_length: int = 1000, save_path: str =
                 sd.wait()
         
         if plot:
-            plot_graph_from_adj(markov_chain, previous_chords=list(prev_state), current_chord=current_chord, mode="simulate")
+            plot_graph_from_adj(markov_chain, previous_chords=list(prev_state), current_chord=current_chord, mode="simulate", temperature=temperature)
 
         prev_state = tuple((list(prev_state)[1:]) + [current_chord])
 
-    wav_write(save_path, 44100, (np.concatenate(audio_sequence)*32767).astype(np.int16))
+    if save:
+        wav_write(save_path, 44100, (np.concatenate(audio_sequence)*32767).astype(np.int16))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fit a markov chain to a music piece, simulate it ans save the resulting composition in a .wav file")
@@ -121,12 +144,14 @@ if __name__ == "__main__":
     parser.add_argument("--input_path", type=str, help="A path to the piece to fit to, which should be a text file containing only space-separated chords. A chord is a comma-separated list of integer frequencies.")
     parser.add_argument("--look_back", type=int, default=1, help="The number of previous chords to condition the next chord on in the learned distribution.")
     parser.add_argument("--play", action="store_true", help="Enable playing the piece while fitting and while simulating?")
-    parser.add_argument("--plot", action="store_true", help="Enable visualising the transition graph while fitting and while simulating?")
-    parser.add_argument("--output_path", type=str, default="output.wav", help="The path to the .wav file to save the musical composition in.")
+    parser.add_argument("--plot", action="store_true", help="Enable visualising the transition graph while fitting and while simulating? (large models need beefy computer!)")
+    parser.add_argument("--save", action="store_true", help="Enable saving the composition to a .wav file?")
+    parser.add_argument("--output_path", type=str, default="output.wav", help="The path to the .wav file to save the musical composition in (iff --save is used)")
     parser.add_argument("--composition_length", type=int, default=3000, help="The length of the generated composition (in chords). 3000 chords is about 10 minutes.")
+    parser.add_argument("--temperature", type=float, default=1.0, help="The temperature parameter for the softmax applied on the conditional frequencies; lower = more deterministic.")
     
     args = parser.parse_args()
     
     crappy_composer = fit_markov_chain(args.input_path, look_back=args.look_back, play=args.play, plot=args.plot)
-    compose_piece(crappy_composer, piece_length=args.composition_length, save_path=args.output_path, play=args.play, plot=args.plot)
+    compose_piece(crappy_composer, piece_length=args.composition_length, save_path=args.output_path, play=args.play, plot=args.plot, save=args.save, temperature=args.temperature)
 
